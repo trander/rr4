13fae20afdfec6b8b4d8145f892ff39ab9eeae94
Hyunjung <trlee329@gmail.com>
2017-08-17 21:58:14 +0200

    setup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import About from './routes/About';
 
-class App extends Component {
-  render() {
+const App = () => {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <Router>
+            <div>
+                <Route exact path="/" component={Home}/>
+                <Route path="/about" component={About}/>
+            </div>
+        </Router>
     );
-  }
-}
+};
 
 export default App;
