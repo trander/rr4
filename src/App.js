a808bac5597ebc9ff8667e600a715a87a0ea71de
Hyunjung <trlee329@gmail.com>
2017-08-17 22:07:07 +0200

    Header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Home from './routes/Home';
 import About from './routes/About';
+import Header from "./components/Header";
 
 const App = () => {
     return (
         <Router>
             <div>
+                <Header />
                 <Route exact path="/" component={Home}/>
                 <Route path="/about" component={About}/>
             </div>
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <Link to="/" className="item"> Home </Link>
+            <Link to="/about" className="item"> About </Link>
+        </div>
+    );
+};
+
+export default Header;
