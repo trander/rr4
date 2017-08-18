5165b60103f1699edc78c2ae78b1320efbc01b45
Hyunjung <trlee329@gmail.com>
2017-08-18 13:40:28 +0200

    redirect
    history.push()

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const Home = () => {
+const Home = ({history}) => {
     return (
         <div>
             Home
+            <button onClick={()=>{history.push('/posts')}}> push </button>
         </div>
     );
 };
